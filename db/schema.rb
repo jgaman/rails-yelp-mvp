f031dc846cb238287c1bd07c9dde71b51041d02f
jean luc Gaman <gaman2404@gmail.com>
2019-08-14 11:54:11 +0200

    Add address to Restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_14_094114) do
+ActiveRecord::Schema.define(version: 2019_08_14_094956) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
